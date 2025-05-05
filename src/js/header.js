edb1490f9584097e5a617b532b9727801ae65a1b
LeBochuliak <elenadolinskaya03@gmail.com>
2025-05-05 23:32:49 +0200

    Add retina-image

--- src/js/header.js
+++ src/js/header.js
@@ -2,10 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const burgerButton = document.querySelector('[data-action="open-menu"]');
     const closeButton = document.querySelector('[data-action="close-menu"]');
     const menuBackground = document.querySelector('[data-menu-background]');
-    const menuLinks = document.querySelectorAll('[data-mobile-link]');
+    const menuLinks = document.querySelectorAll(
+        '[data-mobile-link], [data-nav-link]'
+    );
 
     function openMobileMenu() {
-        console.log('Open menu clicked');
         menuBackground.classList.add('is-open');
         document.body.style.overflow = 'hidden';
     }
@@ -25,22 +26,17 @@ document.addEventListener('DOMContentLoaded', () => {
     menuLinks.forEach(link => {
         link.addEventListener('click', event => {
             event.preventDefault();
-            const targetSection = document.getElementById(
-                link.getAttribute('href').substring(1)
-            );
-            if (targetSection) {
-                targetSection.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start',
-                });
-            }
+            const targetId =
+                link.getAttribute('data-scroll-to') ||
+                link.getAttribute('href').substring(1);
+            scrollToSection(targetId);
             closeMobileMenu();
         });
     });
 
     if (menuBackground) {
         menuBackground.addEventListener('click', event => {
-            if (!event.target.closest('.mobile-menu-wrapper')) {
+            if (!event.target.closest('[data-mobile-wrapper]')) {
                 closeMobileMenu();
             }
         });
@@ -54,8 +50,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function scrollToSection(sectionId) {
         const section = document.getElementById(sectionId);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        const isDesktop = window.innerWidth >= 1200;
+        const offset = isDesktop ? 96 : 50;
+        const offsetInner = isDesktop ? 14 : 0;
+
+        if (sectionId === 'game-description') {
+            const offsetTop =
+                section.getBoundingClientRect().top + window.scrollY - offset;
+            window.scrollTo({ top: offsetTop, behavior: 'smooth' });
+        } else {
+            const offsetTop =
+                section.getBoundingClientRect().top +
+                window.scrollY -
+                offsetInner;
+            window.scrollTo({ top: offsetTop, behavior: 'smooth' });
         }
     }
 });
