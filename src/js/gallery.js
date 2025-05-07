087bc934ec15fed42c61ef21dfbb0916bbb1bbc2
Valya Vovchenko <valya253@ukr.net>
2025-05-08 00:07:39 +0300

    add change

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,25 +1,36 @@
 import Swiper from 'swiper';
 import 'swiper/css';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/navigation';
 
 const prevBtn = document.querySelector('.gallery-btn-prev');
 const nextBtn = document.querySelector('.gallery-btn-next');
 
-const gallerySwiper = new Swiper('.gallery-swiper', {
+function updateButtons(swiper) {
+  prevBtn.disabled = swiper.isBeginning;
+  nextBtn.disabled = swiper.isEnd;
+}
+
+
+const swiper = new Swiper('.gallery-swiper', {
+  modules: [Navigation],
   slidesPerView: 1,
-  spaceBetween: 20,
-  speed: 800,
-  on: {
-    slideChange: function () {
-      prevBtn.disabled = gallerySwiper.isBeginning;
-      nextBtn.disabled = gallerySwiper.isEnd;
-    }
+  spaceBetween: 70,
+  navigation: {
+    nextEl: '.gallery-btn-next',
+    prevEl: '.gallery-btn-prev',
   },
   breakpoints: {
     1200: {
       slidesPerView: 1,
-    }
-  }
+    },
+  },
+  on: {
+    init() {
+      updateButtons(this);
+    },
+    slideChange() {
+      updateButtons(this);
+    },
+  },
 });
-
-prevBtn.addEventListener('click', () => gallerySwiper.slidePrev());
-nextBtn.addEventListener('click', () => gallerySwiper.slideNext());
