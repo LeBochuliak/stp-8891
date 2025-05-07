9adbfc2176b73f50403eaa2a6f730cc24e8d02e0
Valya Vovchenko <valya253@ukr.net>
2025-05-07 22:25:56 +0300

    working arrows added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import './js/header.js';
 import './js/cookies.js';
 import './js/game-description.js';
-
-// import './js/reviews.js';
-
-// import './js/gallery.js';
-
+import './js/reviews.js';
+import './js/gallery.js';
 import './js/main-characters.js';
 
